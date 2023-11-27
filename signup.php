893813a75c18eb7ab449552129defde58ac497af
omarmsragab <omarmsragab2003@gmail.com>
2023-11-26 23:44:50 -0700

    Added a sign up page

--- signup.php
+++ signup.php
@@ -8,7 +8,7 @@
         <div class="onboarding-overlay-outer">
           <div class="onboarding-overlay-inner returning">
             <img class="returning__image" src="images/logo.png" alt="Horse logo">
-            <h1 class="returning__header">Sign in to Farrier&nbsp;Site</h1>
+            <h1 class="returning__header">Sign up to Farrier&nbsp;Site</h1>
             <form class="signin">
               <div class="form-group">
                 <label for="email_or_username">Username</label>
@@ -18,12 +18,16 @@
                 <label for="password">Password</label>
                 <Input type="password" class="form-control" id="password" aria-describedby="emailHelp" />
               </div>
+              <div class="form-group">
+                <label for="confirm_password">Confirm Password</label>
+                <Input type="password" class="form-control" id="confirm_password" aria-describedby="emailHelp" />
+              </div>
               <!-- {{#if error}}
                 <br>
                 <small class="onboarding-form__small"><FaIcon @icon="exclamation-triangle" /> Something's wrong. Please check your email address and password.</small>
               {{/if}} -->
-              <button class="onboarding-form__btn returning__btn" type="submit">Sign in</button>
-              <label class="signup"> Not a member? <a href="signup.php">Sign up</a> now!</label>
+              <button class="onboarding-form__btn returning__btn" type="submit">Sign up</button>
+              <label class="signup"> Already a member? <a href="index.php">Sign in</a> now!</label>
             </form>
           </div>
           <p class="overlay-copyright">&copy;2023 Omar, Aidan, Youssef</p>
