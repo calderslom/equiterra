90bcafd84482b17a220d2e68a7c88eb2d92622b5
omarmsragab <omarmsragab2003@gmail.com>
2023-11-27 18:06:59 -0700

    added barns.php

--- navbar.php
+++ navbar.php
@@ -12,6 +12,7 @@
           // Assuming you have the user type stored in a session variable
           if ($_SESSION['user_type'] == 'Admin') {
             // Display links for admin
+            echo '<a class="'.(basename($_SERVER['PHP_SELF']) == 'barns.php' ? 'active' : '').'" href="barns.php">Barns</a>';
             echo '<a class="'.(basename($_SERVER['PHP_SELF']) == 'customers.php' ? 'active' : '').'" href="customers.php">Customers</a>';
           } else if ($_SESSION['user_type'] == 'Client') {
             // Display links for client
