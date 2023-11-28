f425f10557b61fde1a414d28f584a48b116cf71f
omarmsragab <omarmsragab2003@gmail.com>
2023-11-28 14:29:35 -0700

    improved welcome page

--- home.php
+++ home.php
@@ -6,6 +6,14 @@
     <div class="onboarding-overlay">
       <div class="onboarding-overlay-outer">
         <?php include 'navbar.php'; ?>
+        <img class="welcome-image" src="images/welcome.png" alt="Welcome">
+        <div class="button-container">
+          <a href="horses.php" class="large-button horse">Horses</a>
+          <a href="barns.php" class="large-button barn">Barns</a>
+          <a href="customers.php" class="large-button customer">Customers</a>
+          <a href="account.php" class="large-button account">Account Info</a>
+          <!-- Add more buttons as needed -->
+        </div>
         <p class="overlay-copyright">&copy;2023 Omar, Aidan, Youssef</p>
       </div>
     </div>
---
+++ horse.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  <head>
+  <body>
+    <div class="onboarding-overlay">
+      <div class="onboarding-overlay-outer">
+        <?php include 'navbar.php'; ?>
+        <p class="overlay-copyright">&copy;2023 Omar, Aidan, Youssef</p>
+      </div>
+    </div>
+  </body>
+</html>
