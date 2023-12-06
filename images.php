4904b79149b1444116304229b2a97a9eb7042708
omarmsragab <omarmsragab2003@gmail.com>
2023-12-06 13:47:17 -0700

    added ability to add and remove images as admin from the images page

--- images.php
+++ images.php
@@ -5,8 +5,32 @@ if (session_status() == PHP_SESSION_NONE) {
 if (isset($_GET['horse_name'])) {
   $_SESSION['horse_name'] = urldecode($_GET['horse_name']);
 }
+
+if (isset($_POST['url'])) {
+  if (($key = array_search($_POST['url'], $_SESSION['images'])) !== false) {
+    unset($_SESSION['images'][$key]);
+  }
+}
+
+if (isset($_POST['add_url'])) {
+  if (filter_var($_POST['add_url'], FILTER_VALIDATE_URL)) {
+    array_push($_SESSION['images'], $_POST['add_url']);
+  } else {
+    $error = "Invalid Image URL!";
+  }
+}
 // ...
 ?>
+
+<script>
+function addImageInput() {
+  var addImageDiv = document.getElementById('add-image-div');
+  addImageDiv.innerHTML = "<form method='POST'>" +
+                          "<input type='text' name='add_url' placeholder='Insert image url...' style='width: 1070px; height: 32px; border-radius: 5px; border: 1px solid #ccc; margin-right: 10px;'>" +
+                          "<input type='submit' class='add-button' value='Add'>" +
+                          "</form>";
+}
+</script>
 
 <html>
 
@@ -24,12 +48,28 @@ if (isset($_GET['horse_name'])) {
         <?php
           echo "<h1 class='returning__header'>Images for " . $_SESSION['horse_name'] . "</h1>";
           if (isset($_SESSION['images']) && is_array($_SESSION['images'])) {
+            if ($_SESSION['user_type'] == "Admin") {
+              echo "<div id='add-image-div' style='text-align: right; display: flex; align-items: center; justify-content: flex-end;'>";
+              if (isset($error)) {
+                echo "<p style='color:red; text-align:center; font-size:20px; margin-right: 380px;'>$error</p>";
+              }
+              echo "<button id='add-image-button' class='add-button' onclick='addImageInput()'>Add Image +</button>";
+              echo "</div>";
+            }
             echo "<div class='image-container'>";
             foreach ($_SESSION['images'] as $image_url) {
               if (filter_var($image_url, FILTER_VALIDATE_URL)) {
+                echo "<div class='image-wrapper'>";
                 echo "<a href='" . htmlspecialchars($image_url) . "' target='_blank'>";
                 echo "<img class='horse-image' src='" . htmlspecialchars($image_url) . "' alt='Image'>";
                 echo "</a>";
+                if ($_SESSION['user_type'] == "Admin") {
+                  echo "<form method='POST' onsubmit='return confirm(\"Are you sure you want to delete this image?\");'>";
+                  echo "<input type='hidden' name='url' value='" . htmlspecialchars($image_url) . "'>";
+                  echo "<input type='submit' class='delete-button' value='X'>";
+                  echo "</form>";
+                }
+                echo "</div>";
               } else {
                 echo "<div class='returning__header'>Invalid image URL: " . htmlspecialchars($image_url) . "</div>";
               }
