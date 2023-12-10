f87820dc76caca52d9036320eb6b809da899be88
youssefsamaan2003 <127629577+youssefsamaan2003@users.noreply.github.com>
2023-12-10 16:21:17 -0700

    fixed signout issue

--- signout.php
+++ signout.php
@@ -1,9 +1,17 @@
 <?php
+//starting the session
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+  }
+
 // Unset all of the session variables
 session_unset();
 
 // Destroy the session
 session_destroy();
+
+//unset user type
+unset($_SESSION['user_type']);
 
 // Redirect to the login page
 header('Location: index.php');
